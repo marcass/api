60cd7b2abe1dbed42a665515dcb2966a7c2ac004
marcass <best.nose@gmail.com>
2019-02-20 09:58:47 +1300

    making routes for jwt auth

--- docker/python/init.py
+++ docker/python/init.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from flask import Flask, jsonify, request
 from flask_cors import CORS, cross_origin
+from flask_jwt_extended import JWTManager
 
 app = Flask(__name__)
 CORS(app)
+jwt = JWTManager(app)
--- docker/python/kong-auth-test.py
+++ docker/python/kong-auth-test.py
@@ -7,7 +7,7 @@ import json
 import requests
 from init import app, jwt
 
-app.secret_key = 09q785349hangpoiqa5984  # Change this!
+app.secret_key = '09q785349hangpoiqa5984'  # Change this!
 app.config['JWT_HEADER_TYPE'] = 'Bearer'
 
 @app.route('/auth/login', methods=['POST'])
@@ -18,39 +18,18 @@ def auth():
         print (username)
         print (password)
         # get user detail from kong auth route
-        r = requests.get('http://kong:8001/'+username)
-        print (r.text)
-        ret = {'access_token': create_access_token(identity=user), 'refresh_token': create_refresh_token(identity=user)}
-        return jsonify(ret), 200
-                                                                                                                                                                                                                                                                                    else:
-                                                                                                                                                                                                                                                                                                    # print 'fucked up with a bad username'
-                                                                                                                                                                                                                                                                                                            return jsonify({"msg": "Bad username or password"}), 401
-                                                                                                                                                                                                                                                                                                            except:
-                                                                                                                                                                                                                                                                                                                        print 'empty request'
-                                                                                                                                                                                                                                                                                                                            return jsonify({'Status':'Error', 'Message':'Empty request'})
-
-@app.route("/auth", methods=['POST',])
-def post_test():
-    '''
-    JWT generator test
-    '''
-    print ("headers are:")
-    print (request.headers)
-    content = request.get_json(silent=False)
-    print (content)
-
-    return jsonify(content), 200
-
-@app.route("/auth/hello", methods=['GET',])
-def get_test():
-    '''
-    Get test
-    '''
-    print ("headers are:")
-    print (request.headers)
-    content = {'msg': 'This is what you get with kong'} 
-    print("Hello thre konga")
-    return jsonify(content), 200
+        r = requests.post('http://kong:8000/auth/'+username, json={'username': 'auth', 'password': 'iamauth'})
+        if r.status_code == 200:
+            print (r.text)
+            ret = {'access_token': create_access_token(identity=user), 'refresh_token': create_refresh_token(identity=user)}
+            print (ret)
+            return jsonify(ret), 200
+        else:
+            # print 'fucked up with a bad username'
+            return jsonify({"msg": "Bad username or password"}), 401
+    except:
+        print ('empty request')
+        return jsonify({'Status':'Error', 'Message':'Empty request'})
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0')
