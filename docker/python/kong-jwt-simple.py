d8b87cca45914f121dbdf01223185acd266833e6
Marcass <best.nose@gmail.com>
2019-02-22 20:52:08 +1300

    chnaing jwt manager

---
+++ docker/python/kong-jwt-simple.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+from flask import Flask, request, jsonify
+from flask_jwt_simple import (
+    JWTManager, jwt_required, create_jwt, get_jwt_identity
+)
+import json
+import requests
+from requests.auth import HTTPBasicAuth
+
+app = Flask(__name__)
+
+# Setup the Flask-JWT-Simple extension
+app.config['JWT_SECRET_KEY'] = 'super-secret'  # Change this!
+jwt = JWTManager(app)
+
+app.config['JWT_HEADER_TYPE'] = 'Bearer'
+
+# Using the jwt_data_loader, we can change the values that
+# will be present in the JWTs (that are made by the
+# `create_jwt()` function). This will override everything
+# currently in the token, so you will need to re-add
+# the default claims (exp, iat, nbt, sub) if you still
+# want them.
+@jwt.jwt_data_loader
+def add_claims_to_access_token(identity):
+    print(identity)
+    if identity == 'admin':
+        roles = 'admin'
+    else:
+        roles = 'peasant'
+
+    now = datetime.utcnow()
+    return {
+        'exp': now + current_app.config['JWT_EXPIRES'],
+        'iat': now,
+        'nbf': now,
+        'sub': identity,
+        'roles': roles,
+        'iss': 'skibo.duckdns.org'
+    }
+
+@app.route('/auth/login', methods=['GET', 'POST'])
+def auth():
+    # try:
+    content = request.get_json(silent=False)
+    print(content)
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+    if not username:
+        return jsonify({"msg": "Missing username parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+    # setup something to creat the user variable here
+    print (username)
+    print (password)
+    # user = 'test-user'
+    # return jsonify(content), 200
+    # get group
+    try:
+        x = requests.get('http://localhost:8000/jwt-stuff/'+username+'/acls', auth=HTTPBasicAuth(username, password))
+        if x.status_code == 200:
+            print (x.text)
+            data = json.loads(x.text)
+            group = data['data'][0]['group']
+            print (group)
+        else:
+            # print 'fucked up with a bad username'
+            print(x.status_code)
+            return jsonify({"msg": x.status_code}), 401
+    except:
+        print("couldn't get group")
+        return jsonify({'Status':'Error', 'Message':'No group returned'}), 403
+    try:
+        # get jwt stuff for making token
+        # assumes route setup in kong that allows a path of jwt, with a service that has un upstram path of /consumers
+        r = requests.get('http://localhost:8000/jwt-stuff/'+username+'/jwt', auth=HTTPBasicAuth(username, password))
+        # r = requests.post('http://kong:8000/auth/'+username, json={'username': 'auth', 'password': 'iamauth'})
+        if r.status_code == 200:
+            print (r.text)
+            payload = json.loads(r.text)['data'][0]
+            print (payload)
+        else:
+            # print 'fucked up with a bad username'
+            print(r.status_code)
+            return jsonify({"msg": r.status_code}), 401
+    except:
+        print("couldn't get jwt detail")
+        return jsonify({'Status':'Error', 'Message':'No jwt detail returned'}), 403
+    payload.update({'group': group})
+    print (payload)
+    print (type(payload))
+    # , 'refresh_token': create_refresh_token(identity=user)}
+    ret = {'access_token': create_jwt(identity=payload)}
+    print (ret)
+    return jsonify(ret), 200
+    # except:
+    #     print ('empty request')
+    #     return jsonify({'Status':'Error', 'Message':'Empty request'})
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0')
