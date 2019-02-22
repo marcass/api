290262b05f522f69183840b79323b69b18b234b1
marcass <best.nose@gmail.com>
2019-02-22 22:59:07 +1300

    claims harvesting from jwt is working now. discarded idea of refresh token

--- docker/python/kong-test.py
+++ docker/python/kong-test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 from flask import Flask, request, jsonify
 import json
+import jwt
+
 from init import app
 
 @app.route("/flask/data", methods=['POST',])
@@ -19,127 +21,17 @@ def get_test():
     '''
     Get test
     '''
-    print ("headers are:")
-    print (request.headers)
-    content = {'msg': 'This is what you get with kong'} 
+    token = request.headers['Authorization'].split( )[1]
+    print (token)
+    claims = jwt.decode(token, verify=False)
+    print (claims)
+    roles = claims['roles']
+    print (roles)
+    username = claims['sub']['username']
+    print(username)
+    content = {'msg': 'This is what you get with kong'}
     print("Hello thre konga")
     return jsonify(content), 200
-
-
-# @app.route("/data", methods=['GET',])
-# def get_data():
-#     '''
-#     Get data from influx about sensor types
-#     returns: [{'site': 'marcus',
-#         'location': [{'fields': [u'light', u'temp'], 'id': u'downhall'},
-#                     {'fields': [u'light', u'temp'], 'id': u'kitchen'},
-#                     {'fields': [u'light', u'temp'], 'id': u'lounge'},
-#                     {'fields': [u'light', u'temp'], 'id': u'spare'},
-#                     {'fields': [u'light', u'temp'], 'id': u'window'}]}]
-#     '''
-#     return jsonify({"sensorID": sensors.get_sensorIDs(), "measurements": sensors.get_measurements()}), 200
-#
-#
-# @app.route("/data/values", methods=['POST',])
-# def get_values():
-#     '''
-#     Get data from influx
-#     sends: {"measurement": [{"location": <location1>, "sensors":[{'id': <sens1>, 'type': <temp/hum>}........]},....], "range":<RP to graph from>, "period": int}
-#     returns: traces for plotly
-#     '''
-#     content = request.get_json(silent=False)
-#     # print 'views content is:'
-#     # print content
-#     return jsonify(sensors.start_data(content)), 200
-#
-#
-# @app.route("/data/values/custom", methods=['POST',])
-# def get_cust_values():
-#     '''
-#     Get data from influx
-#     sends: {"measurement": [{"location": <location1>, "sensors":[{'id': <sens1>, 'type': <temp/hum>}........]},....], "range":<RP to graph from>, "period": int}
-#     returns: traces for plotly
-#     '''
-#     content = request.get_json(silent=False)
-#     # print 'views content is:'
-#     # print content
-#     return jsonify(sensors.custom_data(content)), 200
-#
-# @app.route("/data/values/customAx", methods=['POST',])
-# def get_cust_axes():
-#     '''
-#     Get data from influx
-#     sends: {"site": site, "traces": [{"senosorID": "lounge", "site": "julian", "type": "light"}, .....], "range":<RP to graph from>, "period": int}
-#     returns: traces for plotly
-#     '''
-#     content = request.get_json(silent=False)
-#     # print 'views content is:'
-#     # print content
-#     return jsonify(sensors.custom_ax(content)), 200
-#
-# @app.route("/data/values/sites", methods=['GET',])
-# def get_sites():
-#     '''
-#     Get a list of sites
-#     '''
-#     sites = sensors.get_sites()
-#     # Need some way for kong to spit out user here, or change routes dynamically
-#     if user_data['role'] == 'admin':
-#         ret = []
-#         for i in sites[0]:
-#             loc = sites[0].index(i)
-#             ret.append({'sitename':sites[0][loc], 'measurement': sites[1][loc]})
-#         return jsonify(ret), 200
-#     # content = request.get_json(silent=False)
-#     else:
-#         if 'sites' in user_data:
-#             allowed_sites = []
-#             for i in user_data['sites']:
-#                 allowed = json.loads(i)
-#                 if allowed['sitename'] in sites[0]:
-#                     loc = sites[0].index(allowed['sitename'])
-#                     allowed_sites.append({'sitename':sites[0][loc], 'measurement':sites[1][loc]})
-#             return jsonify(allowed_sites), 200
-#
-#
-# @app.route("/data/values/types", methods=['GET',])
-# def get_types():
-#     '''
-#     Get a list of types
-#     '''
-#     return jsonify(sensors.get_data_types()), 200
-#
-# @app.route("/data/values/site/<site>", methods=['GET',])
-# def get_site_data(site):
-#     '''
-#     Get a site data keys without specifying measurement
-#     '''
-#     return jsonify(sensors.get_sensorIDs(site)), 200
-#
-# @app.route("/data/values/site/<measurement>/<site>", methods=['GET',])
-# def get_meas_site_data(measurement, site):
-#     '''
-#     Get a site data keys with a specified measurement
-#     '''
-#     return jsonify(sensors.get_sensorIDs(site, measurement)), 200
-#
-#
-# @app.route("/data/values/type/<Type>", methods=['GET',])
-#
-# def get_type_data(Type):
-#     '''
-#     Get a sensor type data keys
-#     '''
-#     content = request.get_json(silent=False)
-#     return jsonify(sensors.get_type_sensors(Type)), 200
-#
-# @app.route("/data/values/all", methods=['GET',])
-# def get_all_sens():
-#     '''
-#     Get a sensor type data keys
-#     '''
-#     content = request.get_json(silent=False)
-#     return jsonify(sensors.get_all_sensors()), 200
 
 if __name__ == "__main__":
     app.run(host= '0.0.0.0')
