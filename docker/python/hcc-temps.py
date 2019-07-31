e323c596956a51a1466247b246845d9763a7f5e3
Marcass <best.nose@gmail.com>
2019-08-01 10:25:54 +1200

    moved hcc temps to this repo

--- docker/python/hcc-temps.py
+++ docker/python/hcc-temps.py
@@ -28,19 +28,19 @@ AUTH_URL = 'https://skibo.duckdns.org/api/auth/login'
 DATA_URL = 'https://skibo.duckdns.org/api/data'
 headers = ''
 jwt = ''
-jwt_refresh = ''
-refresh_headers = {"Authorization": "Bearer %s" %jwt_refresh}
+#jwt_refresh = ''
+#refresh_headers = {"Authorization": "Bearer %s" %jwt_refresh}
 
 def getToken():
     global jwt
-    global jwt_refresh
+#    global jwt_refresh
     global headers
     r = requests.post(AUTH_URL, json = {'username': creds.user, 'password': creds.password})
     tokens = r.json()
-    #print 'token data is: ' +str(tokens)
+    print ('token data is: ' +str(tokens))
     try:
         jwt = tokens['access_token']
-        jwt_refresh = tokens['refresh_token']
+        #jwt_refresh = tokens['refresh_token']
         headers = {"Authorization":"Bearer %s" %jwt}
         print('got token')
     except:
@@ -48,17 +48,17 @@ def getToken():
 
 def post_data(data):
     global jwt
-    global jwt_refresh
+    #global jwt_refresh
     global headers
     if (jwt == ''):
         print('Getting token')
         getToken()
     ret = requests.post(DATA_URL, json = data, headers = headers)
-    #print 'JWT = '+str(jwt)
-    #print 'First response is: ' +str(ret)
-    # if '200' in str(ret):
-    #     print('Success')
-        # print(data)
+    print ('JWT = '+str(jwt))
+    print 'First response is: ' +str(ret)
+    if '200' in str(ret):
+        print('Success')
+        print(data)
     if '200' not in str(ret):
         print('Oops, not authenticated')
         try:
@@ -87,7 +87,7 @@ def on_message(client, userdata, msg):
                 'sensorID':sensor, 'site': 'marcus'}, 'value':temp}
         post_data(data)
     except:
-        print('unable to fomrat message for posting')
+       print('unable to fomrat message for posting')
 
 
 #subscribe to broker and test for messages below alert values
