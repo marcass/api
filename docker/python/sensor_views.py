0c330892f0c933c2ff787d53156d786f2a40123d
marcass <best.nose@gmail.com>
2019-08-14 11:44:17 +1200

    removed telegram bot from gunicorn worker as it causes a conflict with 2 processes

--- docker/python/sensor_views.py
+++ docker/python/sensor_views.py
@@ -3,8 +3,6 @@ import creds
 import json
 import sensor_data as sensors
 from init import app
-# telegram bots for alerts
-import telegram_tanks
 
 # Sensor data routes ####################################
 @app.route("/data", methods=['POST',])
