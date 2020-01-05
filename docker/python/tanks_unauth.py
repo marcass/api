7779128b9c115fe3efb8976d5617ced936de1447
marcass <best.nose@gmail.com>
2020-01-06 11:42:05 +1300

    added filtering for data from tanks

---
+++ docker/python/tanks_unauth.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+import creds
+import json
+import sensor_data as sensors
+from init import app
+
+# Sensor data routes ####################################
+@app.route("/tanks", methods=['POST',])
+def update_data():
+    '''
+    Writes data to influx from remote sensor
+    '''
+    #print("SEnsor data incoming")
+    # print (request.headers)
+    content = request.get_json(silent=False)
+    print (content)
+    if 'PY' in content and len(content) < 100:
+        return jsonify(sensors.sort_tank_data(content)), 200
+    else:
+        return 412
+
+if __name__ == "__main__":
+    app.run()
