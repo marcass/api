2d585303ce6ea2df5ab7ea5676f60178d03e30ca
marcass <best.nose@gmail.com>
2020-01-07 11:46:00 +1300

    keeping up to daate

--- docker/python/tanks_unauth.py
+++ docker/python/tanks_unauth.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 #import creds
 import json
-# import sensor_data as sensors
+import sensor_data as sensors
 from init import app
 
 # Sensor data routes ####################################
@@ -12,25 +12,25 @@ def update_data():
     '''
     print("SEnsor data incoming")
     # print (request.headers)
-    # print (request.data)
+    print (request.data)
     # content = request.get_json(silent=False)
     content = request.data
     # print (type(content))
     a = content.decode("utf-8", errors='replace')
-    # print(a)
+    print(a)
     # data = json.loads(content.decode("utf-8", errors='replace'))
     b = json.loads(a)
     # stripped = content.encode("ascii", "ignore")
     # print (content)
-    # print (b)
+    print (b)
     # formatted = json.loads(stripped)
     # print (type(stripped))
     # print(stripped)
     # if 'PY' in content and len(content) < 100:
-    try:
-        return jsonify(sensors.sort_tank_data(b)), 200
-    except:
-        return ("Malformed data"), 412
+    #try:
+    return jsonify(sensors.sort_tank_data(b)), 200
+    #except:
+    #    return ("Malformed data"), 412
     # else:
     #     return 412
 
