a98a0e171ef6bf78334b18f19eac636ee9d7fe0d
Marcass <best.nose@gmail.com>
2020-01-06 19:04:30 +1300

    getting data to practice on

--- docker/python/tanks_unauth.py
+++ docker/python/tanks_unauth.py
@@ -16,11 +16,11 @@ def update_data():
     # content = request.get_json(silent=False)
     content = request.data
     print (type(content))
-    stripped = content.encode("ascii", "ignore")
+    # stripped = content.encode("ascii", "ignore")
     print (content)
-    formatted = json.loads(stripped)
-    print (type(stripped))
-    print(stripped)
+    # formatted = json.loads(stripped)
+    # print (type(stripped))
+    # print(stripped)
     # if 'PY' in content and len(content) < 100:
     # return jsonify(sensors.sort_tank_data(content)), 200
     return ("got data"), 200
