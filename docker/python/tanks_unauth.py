ea3c023278b1b73e4fad7f746dc34b7dbfa15c2f
Marcass <best.nose@gmail.com>
2020-01-06 21:36:30 +1300

    getting shit done

--- docker/python/tanks_unauth.py
+++ docker/python/tanks_unauth.py
@@ -15,20 +15,22 @@ def update_data():
     # print (request.data)
     # content = request.get_json(silent=False)
     content = request.data
-    print (type(content))
+    # print (type(content))
     a = content.decode("utf-8", errors='replace')
-    print(a)
+    # print(a)
     # data = json.loads(content.decode("utf-8", errors='replace'))
     b = json.loads(a)
     # stripped = content.encode("ascii", "ignore")
-    print (content)
-    print (b)
+    # print (content)
+    # print (b)
     # formatted = json.loads(stripped)
     # print (type(stripped))
     # print(stripped)
     # if 'PY' in content and len(content) < 100:
-    # return jsonify(sensors.sort_tank_data(content)), 200
-    return ("got data"), 200
+    try:
+        return jsonify(sensors.sort_tank_data(b)), 200
+    except:
+        return ("Malformed data"), 412
     # else:
     #     return 412
 
