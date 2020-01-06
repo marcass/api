2283d20430dfda1eee45d2ae2cf2102e04bdd4c4
marcass <best.nose@gmail.com>
2020-01-06 18:38:51 +1300

    trying to decipher data

--- docker/python/tanks_unauth.py
+++ docker/python/tanks_unauth.py
@@ -1,21 +1,29 @@
 from flask import Flask, request, jsonify
-import creds
+#import creds
 import json
-import sensor_data as sensors
+# import sensor_data as sensors
 from init import app
 
 # Sensor data routes ####################################
-@app.route("/tanks", methods=['POST',])
+@app.route("/marcus", methods=['POST',])
 def update_data():
     '''
     Writes data to influx from remote sensor
     '''
-    #print("SEnsor data incoming")
+    print("SEnsor data incoming")
     # print (request.headers)
-    content = request.get_json(silent=False)
-    # print (content)
+    # print (request.data)
+    # content = request.get_json(silent=False)
+    content = request.data
+    print (type(content))
+    stripped = content.encode("ascii", "ignore")
+    print (content)
+    formatted = json.loads(stripped)
+    print (type(stripped))
+    print(stripped)
     # if 'PY' in content and len(content) < 100:
-    return jsonify(sensors.sort_tank_data(content)), 200
+    # return jsonify(sensors.sort_tank_data(content)), 200
+    return ("got data"), 200
     # else:
     #     return 412
 
