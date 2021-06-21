6d7a043cce589d44a33539193e21ffcb9b09c179
Oracle Public Cloud User <opc@instance-20210615-1141.subnet06151207.vcn06151207.oraclevcn.com>
2021-06-21 05:52:49 +0000

    updated mode of init-letsencrypt

--- docker/nginx/init-letsencrypt.sh
+++ docker/nginx/init-letsencrypt.sh
@@ -10,7 +10,7 @@ domains=(wilsonbay.duckdns.org)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="best.nose@gmail.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
